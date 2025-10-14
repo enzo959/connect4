b28a9d583bac5fb98b201b0e9cd513931f3f5445
couenzo <enzo.courvalet@ynov.com>
2025-10-14 12:07:18 +0200

    ajout_func_reset

--- main.go
+++ main.go
@@ -13,9 +13,14 @@ func gameHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func resetHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	http.HandleFunc("/", startHandler)
 	http.HandleFunc("/game", gameHandler)
+	http.HandleFunc("/reset", resetHandler)
 
 	log.Println("Serveur lancé sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
