3e27293aad29b605dc320a7a40e3a63c34871779
couenzo <enzo.courvalet@ynov.com>
2025-10-16 11:37:13 +0200

    move_func_FullGrid_func_CheckWin_funcTokenMove_in_game.go

--- main.go
+++ main.go
@@ -18,13 +18,14 @@ func TokenMove() {
 
 }
 
-var game *internal.Game
-
 func main() {
 	internal.GameInstance = internal.NewGame()
 
 	http.HandleFunc("/", internal.StartHandler)
 	http.HandleFunc("/game", internal.GameHandler)
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	log.Println("Serveur lancé sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
